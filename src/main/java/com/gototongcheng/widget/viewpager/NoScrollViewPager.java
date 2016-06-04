6278285bfb8d1daf8efb59202ad9cc5a9842158d
郑琰 <zhengyi321@126.com>
2016-06-04 16:31:21 +0800

    走兔安卓端
    走兔安卓端

---
+++ src/main/java/com/gototongcheng/widget/viewpager/NoScrollViewPager.java
@@ -0,0 +1,44 @@
+package com.gototongcheng.widget.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by zhyan on 2016/5/31.
+ */
+public class NoScrollViewPager extends ViewPager {
+
+    private boolean enabled;
+
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = false;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
